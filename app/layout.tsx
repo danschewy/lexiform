48d6f616007842e3b674809eb3f2ab7e2dd58ad2
Daniel <gildenbrand.daniel@gmail.com>
2025-04-10 18:38:25 -0400

    background image

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-[url('/background.jpg')] bg-repeat backdrop-blur-md">
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
